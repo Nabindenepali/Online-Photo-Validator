062e5e077d22ebe73bf1a944e7504d579031d1da
yashasvi <yashpant20@gmail.com>
2019-07-27 15:21:15 +0545

    background_check

--- photo_validator.py
+++ photo_validator.py
@@ -5,6 +5,11 @@ import cv2
 import file_format_check
 import file_size_check
 import blur_check
+import background_check
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
 
 def main():
 	ap = argparse.ArgumentParser()
@@ -16,29 +21,34 @@ def main():
 
 	# Check if the file exists
 	if not os.path.isfile(imgPath):
-		print("The specified file does not exist")
+		logging.info("The specified file does not exist")
 		exit()
 
 	# Check image file format
 	is_file_format_valid = file_format_check.check_image(imgPath)
-	print("File format check: " + ('Passed' if is_file_format_valid else 'Failed'))
+	logging.info("File format check: " + ('Passed' if is_file_format_valid else 'Failed'))
 
 	if not is_file_format_valid:
 		exit()
 
 	# Check image file size
 	is_file_size_valid = file_size_check.check_image(imgPath)
-	print("File size check: " + ('Passed' if is_file_format_valid else 'Failed'))
+	logging.info("File size check: " + ('Passed' if is_file_format_valid else 'Failed'))
 
 	if not is_file_size_valid:
 		exit()
 
-	# Load the image in color
+	# Load the image
 	img = cv2.imread(imgPath)
 
 	# Check image for blurness
 	is_blur = blur_check.check_image(img)
-	print("Blurness check: " + ('Passed' if not is_blur else 'Failed'))
+	logging.info("Blurness check: " + ('Passed' if not is_blur else 'Failed'))
+
+	# Check the background of image
+	is_background_ok = background_check.background_check(img)
+	logging.info("Background check: " + ('Passed' if is_background_ok else 'Failed'))
+
 
 	# Display the imported image
 	cv2.imshow('Application Photo', img)
